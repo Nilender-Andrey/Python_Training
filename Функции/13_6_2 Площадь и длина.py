f36d8fbd0f148548e3f40110aead6db1e46a523f
Nilender-Andrey <Nilenderav@mail.ru>
2022-12-29 07:45:51 +0400

    feat: add task 13_6_2

---
+++ Функции/13_6_2 Площадь и длина.py
@@ -0,0 +1,14 @@
+# Напишите функцию get_circle(radius), которая принимает в качестве аргумента радиус окружности
+# и возвращает два значения: длину окружности и площадь круга, ограниченного данной окружностью.
+
+from math import pi
+
+
+def get_circle(radius):
+    return 2*pi*radius, pi*radius**2
+
+
+r = float(input())
+
+length, square = get_circle(r)
+print(length, square)
